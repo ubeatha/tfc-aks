e0e5c5ba1424b07256d9005848cb94222b502aa7
Andrew <20050899+ubeatha@users.noreply.github.com>
2021-10-11 13:13:46 -0400

    Pointing to release module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "tfm-az-basics" {
-  source      = "git@github.com:ubeatha/tfm-az-basics.git?ref=initial"
+  source      = "git@github.com:ubeatha/tfm-az-basics.git?ref=v0.1.0"
   base_name   = var.base_name
   location    = var.location
   environment = var.environment
