8523cb7b7517254b3e814e2ff8469995f2a150a7
Andrew <20050899+ubeatha@users.noreply.github.com>
2021-10-11 17:24:42 -0400

    Adding AKS and outputs

---
+++ outputs.tf
@@ -0,0 +1,39 @@
+output "resource_group_name" {
+  value = module.az-basics.resource_group_name
+}
+
+output "resource_group_id" {
+  value = module.az-basics.resource_group_id
+}
+
+output "storage_account_name" {
+  value = module.az-basics.storage_account_name
+}
+
+output "storage_account_id" {
+  value = module.az-basics.storage_account_id
+}
+
+output "log_analytics_workspace_name" {
+  value = module.az-basics.log_analytics_workspace_name
+}
+
+output "log_analytics_workspace_id" {
+  value = module.az-basics.log_analytics_workspace_id
+}
+
+output "log_analytics_solution_id" {
+  value = module.az-basics.log_analytics_solution_id
+}
+
+output "group_admin_id" {
+  value = module.az-basics.group_admin_id
+}
+
+output "group_dev_id" {
+  value = module.az-basics.group_dev_id
+}
+
+output "group_test_id" {
+  value = module.az-basics.group_test_id
+}
